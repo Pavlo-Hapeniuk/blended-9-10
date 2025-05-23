102ac4c1adb93a49ae8cad06022264b851fd6051
Pavlo Hapeniuk <johnybfrank@gmail.com>
2025-05-23 16:33:53 +0300

    Update local-storage-api.js

--- src/js/local-storage-api.js
+++ src/js/local-storage-api.js
@@ -1,28 +1,26 @@
 import { taskList } from './refs';
 
-export function saveTaskToLocalStorage(task) {
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  tasks.push(task);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+export function saveTaskLlocalStorage(task) {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
 export function deleteTaskFromLocalStorage(indexEl) {
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  const filteredEl = tasks.filter((task, index) => index !== indexEl);
-  localStorage.setItem('tasks', JSON.stringify(filteredEl));
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const filterEl = tasks.filter((task, index) => index !== indexEl);
+    localStorage.setItem("tasks", JSON.stringify(filterEl));
 }
 
-export function renderTaskFromLocalStorage() {
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  const markup = tasks
-    .map(
-      ({ title, description }) =>
+export function renderTaskLocalStorage() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const marcup = tasks
+        .map(({ title, description }) =>
         `<li class="task-list-item">
-          <button class="task-list-item-btn">Delete</button>
-          <h3>${title}</h3>
-          <p>${description}</p>
-        </li>`
+        <button class="task-list-item-btn">Delete</button>
+        <h3>${title}</h3>
+        <p>${description}</p>
+      </li>`
     )
-    .join('');
-  taskList.insertAdjacentHTML('beforeend', markup);
-}
+        .join('');
+    taskList.insertAdjacentHTML('beforeend', marcup);
+};
