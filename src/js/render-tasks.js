f354d425403018ec6cf96340e641ec310cb8d7c1
Pavlo Hapeniuk <johnybfrank@gmail.com>
2025-05-23 16:35:06 +0300

    Update render-tasks.js

--- src/js/render-tasks.js
+++ src/js/render-tasks.js
@@ -1,35 +1,43 @@
-import { formElement, buttonEl, taskList } from './refs';
-import { renderTask } from './tasks';
-import {
-  saveTaskToLocalStorage,
-  deleteTaskFromLocalStorage,
-  renderTaskFromLocalStorage,
-} from './local-storage-api';
+import { formElement, buttonE1, taskList } from './refs';
+import { randerTask } from './tasks';
+import { saveTaskLlocalStorage, deleteTaskFromLocalStorage, renderTaskLocalStorage} from './local-storage-api';
+ 
+renderTaskLocalStorage()
+formElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const taskName = event.target.taskName.value.trim();
+    const taskDescription = event.target.taskDescription.value.trim();
+    console.log(taskName);
+    console.log(taskDescription);
 
-renderTaskFromLocalStorage();
-
-formElement.addEventListener('submit', event => {
-  event.preventDefault();
-
-  const taskName = event.target.taskName.value.trim();
-  const taskDescription = event.target.taskDescription.value.trim();
-  console.log(taskName);
-  console.log(taskDescription);
-
-  if (!taskName || !taskDescription) {
-    return alert('Заповніть всі поля форми');
-  }
-  renderTask({ title: taskName, description: taskDescription });
-  saveTaskToLocalStorage({ title: taskName, description: taskDescription });
-  formElement.reset();
+    if (!taskName || !taskDescription) {
+        return alert("Заповніть всі поля форми");
+    }
+    randerTask({ title: taskName, description: taskDescription });
+    saveTaskLlocalStorage({title: taskName, description: taskDescription});
+    formElement.reset();
 });
 
-taskList.addEventListener('click', event => {
-  if (event.target.classList.contains('task-list-item-btn')) {
-    const taskItem = event.target.closest('.task-list-item');
-    console.log(taskItem);
-    const indexEl = [...taskList.children].indexOf(taskItem);
-    deleteTaskFromLocalStorage(indexEl);
-    taskItem.remove();
-  }
+taskList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('task-list-item-btn')) {
+        const taskItem = event.target.closest('.task-list-item');
+        console.log(taskItem);
+        const indexEl = [...taskList.children].indexOf(taskItem);
+        deleteTaskFromLocalStorage(indexEl);
+        taskItem.remove();
+    }
+
 });
+
+buttonE1.addEventListener('click', (e) => {
+    const body = document.querySelector("body");
+
+ if (body.classList.contains("theme-dark")) {
+     body.classList.replace("theme-dark", "theme-light")
+     localStorage.setItem("theme", "theme-light");
+
+  } else {
+     body.classList.replace("theme-light", "theme-dark")
+     localStorage.setItem("theme", "theme-dark");
+  }
+})
