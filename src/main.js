feb4e6a516b9295c45c3a1607b049fa9f0845397
Pavlo Hapeniuk <johnybfrank@gmail.com>
2025-05-23 16:38:19 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
-/*
-  Створи список справ.
-  На сторінці є два інпути які має вводиться назва і текст задачі.
-  Після натискання на кнопку "Add" завдання додається до списку #task-list.
-
-  У кожної картки має бути кнопка "Delete", щоб можна було
-  прибрати завдання зі списку.
-  Список із завданнями має бути доступним після перезавантаження сторінки.
-
-  Розмітка картки задачі
-  <li class="task-list-item">
-      <button class="task-list-item-btn">Delete</button>
-      <h3>Заголовок</h3>
-      <p>Текст</p>
-  </li>
-*/
-
 import './js/local-storage-api';
 import './js/markup-tasks';
 import './js/refs';
 import './js/render-tasks';
 import './js/tasks';
 import './js/theme-switcher';
+
+// changeTheme
+// При завантаженні сторінки
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  const body = document.querySelector("body");
+
+  if (savedTheme) {
+    body.classList.remove("theme-dark", "theme-light"); // видаляємо обидві теми
+    body.classList.add(savedTheme); // додаємо збережену
+  }
+});
