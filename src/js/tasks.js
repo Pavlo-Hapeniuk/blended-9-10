8a372342cb381f508b1694280d46bfc8a9d03108
Pavlo Hapeniuk <johnybfrank@gmail.com>
2025-05-23 16:35:35 +0300

    Update tasks.js

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,9 +1,10 @@
 import { taskList } from './refs';
-export function renderTask({ title, description }) {
-  const markup = `<li class="task-list-item">
-          <button class="task-list-item-btn">Delete</button>
-          <h3>${title}</h3>
-          <p>${description}</p>
-        </li>`;
-  taskList.insertAdjacentHTML('beforeend', markup);
+
+export function randerTask({ title, description }) {
+    const marcup = `<li class="task-list-item">
+        <button class="task-list-item-btn">Delete</button>
+        <h3>${title}</h3>
+        <p>${description}</p>
+      </li>`;
+    taskList.insertAdjacentHTML('beforeend', marcup);
 }
